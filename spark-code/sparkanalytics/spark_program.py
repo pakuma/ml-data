cc3c8b655e7ec6ba58252ae7c6a4d3f0002a3fc5
Parveen Kumar <parveen.kumar6@gmail.com>
2020-09-20 00:36:16 -0700

    spark3

--- spark-code/sparkanalytics/spark_program.py
+++ spark-code/sparkanalytics/spark_program.py
@@ -1,2 +1,18 @@
-from pyspark import SparkConf, SparkContext
+import os
+import sys
 
+os.environ['SPARK_HOME'] = "/usr/local/spark/spark-3.0.0-bin-hadoop2.7"
+sys.path.append("/usr/local/spark/spark-3.0.0-bin-hadoop2.7/python/lib/pyspark.zip/pyspark")
+
+from pyspark.sql import SparkSession
+
+spark = SparkSession.builder.appName("People Example").getOrCreate()
+# peopleDF = spark.read.format("csv").option("header", "true") \
+#    .load("/home/parveen/newland/data/people.csv")
+# peopleDF.show()
+# peopleDF.select("name", "age").show()
+peopleJSON = spark.read.format("csv").option("header", "true")\
+    .load("/home/parveen/newland/data/people.csv", sep=";", inferschema="true")
+peopleJSON.show()
+peopleJSON.write.save("/home/parveen/newland/data/people_check.json", format="json")
+# peopleDF.printSchema()
